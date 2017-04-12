4affc5d589ee481ed521e1b84f5eb89338fc084d
SoutheastStar <robknox@outlook.com>
2017-04-12 19:02:09 +1000

    Error Updates

--- BME280.py
+++ BME280.py
@@ -73,7 +73,7 @@ class BME280(object):
         self._addr = addr
 
         self._load_calibration()
-        self._addr.writeByte(self._addr, BME280_REGISTER_CONTROL, 0x3F)
+        #self._i2c.writeByte(0x76, 0xF4, 0x3F)
         self.t_fine =0.0
 
 
@@ -245,8 +245,8 @@ class BME280(object):
         var1 = ((UT) / 16384.0 - (self.dig_T1) / 1024.0) * (self.dig_T2)
         var2 = (((UT) / 131072.0 - (self.dig_T1) / 8192.0) * ((UT)/131072.0 - (self.dig_T1)/8192.0)) * (self.dig_T3)
         self.t_fine = (var1 + var2)
-        cTemp = (var1 + var2) / 5120.0
-        fTemp = cTemp * 1.8 + 32
+        tempC = (var1 + var2) / 5120.0
+        tempF = tempC * 1.8 + 32
         return tempC
     
     def read_temperatureF(self):
@@ -254,8 +254,8 @@ class BME280(object):
         var1 = ((adc_t) / 16384.0 - (self.dig_T1) / 1024.0) * (self.dig_T2)
         var2 = (((adc_t) / 131072.0 - (self.dig_T1) / 8192.0) * ((self.adc_t)/131072.0 - (self.dig_T1)/8192.0)) * (self.dig_T3)
         self.t_fine = (var1 + var2)
-        cTemp = (var1 + var2) / 5120.0
-        fTemp = cTemp * 1.8 + 32
+        tempC = (var1 + var2) / 5120.0
+        tempF = tempC * 1.8 + 32
         return tempF
 
     def read_pressure(self):
@@ -264,19 +264,19 @@ class BME280(object):
         var2 = var1 * var1 * (self.dig_P6) / 32768.0
         var2 = var2 + var1 * (self.dig_P5) * 2.0
         var2 = (var2 / 4.0) + ((self.dig_P4) * 65536.0)
-        var1 = ((dig_P3) * var1 * var1 / 524288.0 + ( self.dig_P2) * var1) / 524288.0
+        var1 = ((self.dig_P3) * var1 * var1 / 524288.0 + ( self.dig_P2) * var1) / 524288.0
         var1 = (1.0 + var1 / 32768.0) * (self.dig_P1)
         p = 1048576.0 - adc
         p = (p - (var2 / 4096.0)) * 6250.0 / var1
         var1 = (self.dig_P9) * p * p / 2147483648.0
         var2 = p * (self.dig_P8) / 32768.0
-        pressure = (p + (var1 + var2 + (self.dig_P7)) / 16.0) / 100
+        pressure = p + (var1 + var2 + (self.dig_P7)) / 16.0
         return pressure
 
     def read_humidity(self):
         adc = self.read_raw_humidity()
         var_H = ((self.t_fine) - 76800.0)
-        var_H = (adc - (self.dig_H4 * 64.0 + self.dig_H5 / 16384.0 * var_H)) * (self.dig_H2 / 65536.0 * (1.0 + self.dig_H6 / 67108864.0 * var_H * (1.0 + self.dig_H3 / 67108864.0 * self.var_H)))
+        var_H = (adc - (self.dig_H4 * 64.0 + self.dig_H5 / 16384.0 * var_H)) * (self.dig_H2 / 65536.0 * (1.0 + self.dig_H6 / 67108864.0 * var_H * (1.0 + self.dig_H3 / 67108864.0 * var_H)))
         humidity = var_H * (1.0 -  self.dig_H1 * var_H / 524288.0)
         if humidity > 100.0 :
             humidity = 100.0
--- BME280_Example.py
+++ BME280_Example.py
@@ -2,12 +2,13 @@ import BME280
 
 sensor = BME280.BME280()
 
-degrees = sensor.read_temperatureC()
+degreesC = sensor.read_temperatureC()
+degreesF = sensor.read_temperatureF()
 pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 humidity = sensor.read_humidity()
 
-print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
-print 'Temp      = {0:0.3f} deg C'.format(degrees)
+print 'Temp      = {0:0.3f} deg C'.format(degreesC)
+print 'Temp      = {0:0.3f} deg F'.format(degreesF)
 print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
 print 'Humidity  = {0:0.2f} %'.format(humidity)
